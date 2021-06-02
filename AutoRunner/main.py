191e7ff0ff3deacf8195ac4c98f00fb09de52f75
Bradley <40966735+BradleyNewell@users.noreply.github.com>
2021-06-02 07:31:52 +0100

    Fix for whitespace in filepaths
    Now correctly handles filepaths containing whitespace

--- AutoRunner/main.py
+++ AutoRunner/main.py
@@ -1,11 +1,14 @@
 import os
+''' TODO: data folder with blank csv file
+'''
+paths_to_exe = [r'"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"',
+                r'"C:\Program Files\Mozilla Firefox\firefox.exe"']
 
-paths_to_exe = [r'C:\"Program Files"\BraveSoftware\Brave-Browser\Application\brave.exe',
-                r'C:\"Program Files"\"Mozilla Firefox"\firefox.exe']
 
-for i in paths_to_exe:
-    try:
-        os.system(i)
-    except FileNotFoundError:
-        print(f"File {str(i)} not found")
+def execute():
+    for i in paths_to_exe:
+        try:
+            os.system(i)
+        except FileNotFoundError:
+            print(f"File {str(i)} not found")
 
